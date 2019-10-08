9842d826161f9410fa58081185a62329170417e0
KennethDyerHill <52476371+KennethDyerHill@users.noreply.github.com>
2019-10-08 15:21:01 -0400

    Update

--- src/App.js
+++ src/App.js
@@ -7,36 +7,39 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
+// Context Imports
+import ProductContext from './contexts/ProductContext';
+
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
 
 	const addItem = item => {
 		// add the given item to the cart
+		setCart([...cart, item]);
 	};
+
+	console.log(cart, products);
 
 	return (
 		<div className="App">
 			<Navigation cart={cart} />
-
+			<ProductContext.Provider
+					value={{
+						products,
+						addItem
+					}}>
 			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
-			/>
+			<Route exact path="/" component={Products} />
+				)};
 
 			<Route
 				path="/cart"
 				render={() => <ShoppingCart cart={cart} />}
 			/>
+			</ProductContext.Provider>
 		</div>
 	);
-}
+				}
 
 export default App;
